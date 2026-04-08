6ce0dabafd52629374258704c375a6496f6e6661
Dhruv Sharma <161147598+Dhruv6september@users.noreply.github.com>
2026-04-08 08:30:50 +0530

    Using HashMap

---
+++ Sumofbeautyofallsubstring.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int beautySum(String s) {
+        int n=s.length();
+        int sum=0;
+        for(int i=0; i<n; i++)
+        {
+            HashMap<Character,Integer> freq=new HashMap<>();
+            for(int j=i; j<n; j++)
+            {
+                freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j),0)+1);
+                int max=Integer.MIN_VALUE;
+                int min=Integer.MAX_VALUE;
+                for(int val: freq.values())
+                {
+                    max=Math.max(max,val);
+                    min=Math.min(min,val);
+                }
+                sum=sum+(max-min);
+            }
+        }
+        return sum;
+    }
+}
